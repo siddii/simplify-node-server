581e3e221b760f7bc96eb8364e8bb0d63c32e05f
Siddique Hameed <siddique_hameed@mastercard.com>
2015-12-11 12:44:46 -0600

    Adding DB

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+var pg = require('pg');
+
 var cool = require('cool-ascii-faces');
 
 app.set('port', (process.env.PORT || 5000));
@@ -18,6 +20,18 @@ app.get('/', function(request, response) {
 app.get('/cool', function(request, response) {
     response.send(cool());
 });
+
+app.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM test_table', function(err, result) {
+            done();
+            if (err)
+            { console.error(err); response.send("Error " + err); }
+            else
+            { response.render('pages/db', {results: result.rows} ); }
+        });
+    });
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
