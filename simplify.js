f1c8151471c1a3b601f98c1c70a8a7981da7ce65
Siddique Hameed <siddique_hameed@mastercard.com>
2015-12-14 13:51:50 -0600

    Adding env variable

--- simplify.js
+++ simplify.js
@@ -3,5 +3,5 @@ var Simplify = require("simplify-commerce");
 
 exports.client = Simplify.getClient({
     publicKey: process.env.SIMPLIFY_API_PUBLIC_KEY,
-    privateKey: process.env.SIMPLIFY_API_PRI
+    privateKey: process.env.SIMPLIFY_API_PRIVATE_KEY
 });
