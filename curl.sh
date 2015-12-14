95c9c0dc8fe9db28501080ff809400936858f121
Siddique Hameed <siddique_hameed@mastercard.com>
2015-12-14 14:06:49 -0600

    Update curl script

--- curl.sh
+++ curl.sh
@@ -1,5 +1,13 @@
+# List customers
+https://simplify-qr-node.herokuapp.com/customers
+
 # Create customer
-curl -H "Content-Type: application/json" -X POST -d '{"name":"John Doe","email":"john.doe@email.com", "card": {"number": "5555555555554444", "expMonth": 12, "expYear": 19}}' http://localhost:5000/addCustomer
+curl -H "Content-Type: application/json" -X POST -d '{"name":"John Doe","email":"john.doe@email.com", "card": {"number": "5555555555554444", "expMonth": 12, "expYear": 19}}' https://simplify-qr-node.herokuapp.com/addCustomer
 
 # Make payments
-curl -H "Content-Type: application/json" -X POST -d '{"amount":155,"customer":"AXd7r98z", "description": "Test payment from node instance"}' http://localhost:5000/pay
+# Make sure the id, referenced here is what we got from the last call
+curl -H "Content-Type: application/json" -X POST -d '{"amount":155,"customer":"AXd7r98z", "description": "Test payment from node instance"}' https://simplify-qr-node.herokuapp.com/pay
+
+#List payments
+https://simplify-qr-node.herokuapp.com/payments
+
